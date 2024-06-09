980e4f60343bb8122c9a7956d5b401cd7d04e89c
Robin <rdunn10@mail.pima.edu>
2024-06-09 09:38:48 -0700

    Update cis129_lab02_shortStory.py
    I changed curly quotes to straight quotes.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -2,7 +2,7 @@
 # My first Python lab in CIS129
 # It’s a story!
 
-print(‘Once upon a time, in the deserts of Arizona, there lived a small boll weevil who was troubled with strange dreams.’)
-print(‘Every night he awoke upon his barrel cactus, staring at the moon, wondering what the faces in his dream had meant.’) 
-print(‘Who was trying to communicate with him? And what exactly had they said to him?’)
-print(‘He couldn’t remember. All he knew was that he was alone in the desert, and someone was watching him.’)
+print('Once upon a time, in the deserts of Arizona, there lived a small boll weevil who was troubled with strange dreams.')
+print('Every night he awoke upon his barrel cactus, staring at the moon, wondering what the faces in his dream had meant.') 
+print('Who was trying to communicate with him? And what exactly had they said to him?')
+print('He couldn't remember. All he knew was that he was alone in the desert, and someone was watching him.')
